b1bf60d8aa214de3d4d092defb595273910dcd9d
姚晟 <yao_luolong@163.com>
2019-08-01 11:05:41 +0800

    'prepared'

--- src/pages/homepage/home.vue
+++ src/pages/homepage/home.vue
@@ -1,17 +1,13 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    <h1>Hello World</h1>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
-
 <style>
-#app {
 
-}
 </style>
